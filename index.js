3f3374d826d2de538e364a3cba45e820dec68011
Elsssqh <elss.hqh@gmail.com>
2024-03-20 14:14:09 +0700

    landing

--- index.js
+++ index.js
@@ -3,42 +3,42 @@ const app = express();
 const port = 3000;
 const expressLayouts = require('express-ejs-layouts');
 
-//ejs
+// Set view engine to ejs
 app.set('view engine', 'ejs');
 
-// layout
+// Use expressLayouts
 app.use(expressLayouts);
 
-//static express
+// Serve static files from public directory
 app.use(express.static('public'));
 
-app.get('/landing', (req, res) => {
-    res.render('landing', { title: 'landing', layout:'landing' });
-})
-
+// Render landing page for root URL
 app.get('/', (req, res) => {
-    res.render('home', { title: 'Home',layout:'home' });
-})
+    res.render('landing', { title: 'Landing', layout: 'landing' });
+});
 
+// Other routes
 app.get('/home', (req, res) => {
-    res.render('home', { title: 'Home',layout:'home' });
-})
+    res.render('home', { title: 'Home', layout: 'home' });
+});
 
 app.get('/about', (req, res) => {
-    res.render('About', { title: 'About',layout:'home' });
-})
+    res.render('about', { title: 'About', layout: 'home' });
+});
 
 app.get('/ourBooks', (req, res) => {
-    res.render('ourBooks', { title: 'ourBooks',layout:'ourBooks' });
-})
+    res.render('ourBooks', { title: 'Our Books', layout: 'ourBooks' });
+});
 
 app.get('/search', (req, res) => {
-    res.render('search', { title: 'search',layout:'search' });
-})
-app.get('/trolley', (req, res) => {
-    res.render('trolley', { title: 'trolley',layout:'trolley' });
-})
+    res.render('search', { title: 'Search', layout: 'search' });
+});
 
+app.get('/trolley', (req, res) => {
+    res.render('trolley', { title: 'Trolley', layout: 'trolley' });
+});
+
+// Start the server
 app.listen(port, () => {
-    console.log(`server running at http://localhost:${port}`);
-})
+    console.log(`Server running at http://localhost:${port}`);
+});
